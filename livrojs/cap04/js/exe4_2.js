1623ef2d9a3fd1a89757f6f746041d198730e5c0
ThiagoSilva <thiagoantonio2108@gmail.com>
2024-10-03 20:09:09 -0300

    Calculando Peso Ideal

--- livrojs/cap04/js/exe4_2.js
+++ livrojs/cap04/js/exe4_2.js
@@ -4,14 +4,17 @@ function calcular() {
     const masc = document.getElementById("inMasculino").Checked;
     const alt = document.getElementById("inAltura").value;
 
-    let peso;
-
-    if (masc) {
-        peso = 22 * Math.pow(alt, 2);
-    } else {
-        peso = 21 * Math.pow(alt, 2);
-    }
-    document.querySelector("h3").innerText = `Peso ideal ${peso.toFixed(2)}`;
-
+    let peso = masc ? Math.pow(alt, 2) * 22 : Math.pow(alt, 2) * 21;
+    /*
+        if (masc) {
+            peso = 22 * Math.pow(alt, 2);
+        } else {
+            peso = 21 * Math.pow(alt, 2);
+        }
+    */
+    document.querySelector("h3").innerText = `${nome} seu Peso ideal é ${peso.toFixed(2)}Kg`;
     return false;
+}
+function limpar() {
+    document.querySelector("h3").innerText = "";
 }
