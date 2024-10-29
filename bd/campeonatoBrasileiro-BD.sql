4be334e1f18e7625b10e6e947590a95737916580
ThiagoSilva <thiagoantonio2108@gmail.com>
2024-10-29 20:16:49 -0300

    Tabela time

--- bd/campeonatoBrasileiro-BD.sql
+++ bd/campeonatoBrasileiro-BD.sql
@@ -12,5 +12,15 @@ create table estadio(
 );
 
 select * from estadio;
-
 desc estadio;
+
+create table time (
+    id_time int,
+    nome_completo varchar(150),
+    nome varchar(150),
+    sigla varchar(3),
+    cidade varchar(150),
+    estado varchar(150),
+    constraint PK_time primary key (id_time),
+    constraint FK_estadioTime foreign key (id_estadio) references estadio(id_estadio)
+);
