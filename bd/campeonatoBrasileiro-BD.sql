02903cae638408290f9e123f33e41105d41a8128
ThiagoSilva <thiagoantonio2108@gmail.com>
2024-10-29 21:48:24 -0300

    Database campeonatoBrasileiro

--- bd/campeonatoBrasileiro-BD.sql
+++ bd/campeonatoBrasileiro-BD.sql
@@ -26,7 +26,7 @@ create table time (
     estado varchar(150),
     id_estadio int not null,
         constraint PK_time primary key(id_time),
-        constraint FK_estadioTime foreign key(id_estadio) references estadio(id_estadio)
+        constraint FK_estadio_time foreign key(id_estadio) references estadio(id_estadio)
 );
 
 create table jogador (
@@ -37,7 +37,7 @@ create table jogador (
     posicao varchar(150),
     id_time int not null,
         constraint PK_jogador primary key(id_jogador),
-        constraint FK_jogadorTime foreign key(id_time) references time(id_time)
+        constraint FK_jogador_time foreign key(id_time) references time(id_time)
 );
 
 create table partida (
@@ -50,7 +50,7 @@ create table partida (
     id_mandante int not null,
     id_visitante int not null,
         constraint PK_partida primary key(id_partida),
-        constraint FK_estadio foreign key(id_estadio) references estadio(id_estadio),
+        constraint FK_partida_estadio foreign key(id_estadio) references estadio(id_estadio),
         constraint FK_time_mandante foreign key(id_mandante) references time(id_time),
         constraint FK_time_visitante foreign key(id_visitante) references time(id_time)
 );
