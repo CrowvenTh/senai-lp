34932559202227a24649c23b28701f221731d8a2
ThiagoSilva <thiagoantonio2108@gmail.com>
2024-10-31 21:22:54 -0300

    Exercicio de banco de dados dbloja, dbmaternidade

--- bd/Exercicios/SQL_DDL_CREATE_TABLE/dbmaternidade.sql
+++ bd/Exercicios/SQL_DDL_CREATE_TABLE/dbmaternidade.sql
@@ -1,3 +1,5 @@
+-- drop database dbmaternidade;
+
 create schema if not exists dbmaternidade default character set utf8;
 use dbmaternidade;
 
@@ -23,5 +25,22 @@ create table if not exists medico (
 		constraint PK_medico primary key(crm)
 )Engine = InnoDB;
 
-select * from medico;
-desc medico;
+-- select * from medico;
+-- desc medico;
+
+create table if not exists bebe (
+	id_bebe int auto_increment,
+    nome varchar(100),
+    dt_nascimento date,
+    peso decimal(5,2),
+    altura int,
+    crm int not null,
+    id_mae int not null,
+		constraint PK_bebe primary key (id_bebe),
+		constraint FK_medico foreign key(crm) references medico(crm),
+        constraint FK_mae foreign key (id_mae) references mae(id_mae)
+)Engine = InnoDB;
+
+
+select * from bebe;
+desc bebe;
