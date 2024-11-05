2eb87a380e8b278dd47b5df43c146d973bcefebc
ThiagoSilva <thiagoantonio2108@gmail.com>
2024-11-04 21:47:20 -0300

    Insert com mySQL

---
+++ bd/Exercicios/campeonatoBrasileiro/InsertoBD.sql
@@ -0,0 +1,47 @@
+create database campeonatoBrasileiro;
+
+use campeonatoBrasileiro;
+
+insert into estadio(nome, endereco, capacidade)
+value ('Allianz Parque','Av. Francisco Matarazzo, 1705 - Água Branca, São Paulo - SP', 43713);
+
+insert into estadio(nome, endereco, capacidade)
+value('Arena Pantanal','Av. Agrícola Paes de Barros - Verdão, Cuiabá - MT',44000);
+
+insert into estadio(nome, endereco, capacidade)
+value('Morumbi','Praça Roberto Gomes Pedrosa, 1 - Morumbi, São Paulo - SP',66795);
+
+insert into estadio(nome, endereco, capacidade)
+value('Arena do Grêmio','Av. Padre Leopoldo Brentano, 110 - Humaitá, Porto Alegre - RS',55662);
+
+insert into estadio(nome, endereco, capacidade)
+value ('Allianz Parque','Av. Francisco Matarazzo, 1705 - Água Branca, São Paulo - SP', 43713);
+
+insert into estadio(nome, endereco, capacidade)
+value('Arena Pantanal','Av. Agrícola Paes de Barros - Verdão, Cuiabá - MT',44000);
+
+insert into estadio(nome, endereco, capacidade)
+value('Morumbi','Praça Roberto Gomes Pedrosa, 1 - Morumbi, São Paulo - SP',66795);
+
+insert into estadio(nome, endereco, capacidade)
+value('Arena do Grêmio','Av. Padre Leopoldo Brentano, 110 - Humaitá, Porto Alegre - RS',55662);
+
+insert into estadio(nome, endereco, capacidade)
+value('Mineirão','Av. Antônio Abrahão Caram, 1001 - Pampulha, Belo Horizonte - MG',61846);
+
+insert into estadio(nome, endereco, capacidade)
+value('Estádio Nilton Santos (Engenhão)', 'R. José dos Reis, 425 - Engenho de Dentro, Rio de Janeiro - RJ',46831);
+
+insert into estadio (nome, endereco, capacidade) values
+('Arena da Baixada (Estádio Joaquim Américo)','R. Buenos Aires, 1260 - Água Verde, Curitiba - PR', 42372);
+
+insert into estadio(nome, endereco, capacidade)
+value('Mineirão','Av. Antônio Abrahão Caram, 1001 - Pampulha, Belo Horizonte - MG',61846);
+
+insert into estadio(nome, endereco, capacidade)
+value('Estádio Nilton Santos (Engenhão)', 'R. José dos Reis, 425 - Engenho de Dentro, Rio de Janeiro - RJ',46831);
+
+insert into estadio (nome, endereco, capacidade) values
+('Arena da Baixada (Estádio Joaquim Américo)','R. Buenos Aires, 1260 - Água Verde, Curitiba - PR', 42372);
+
+select * from estadio;
