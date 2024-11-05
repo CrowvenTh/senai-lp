cb159d9ce86ad0fa494e6fc10a0813c96dc4af31
ThiagoSilva <thiagoantonio2108@gmail.com>
2024-11-04 21:24:05 -0300

    Insert em tabelas com mySQL

---
+++ bd/InsertoBD.sql
@@ -0,0 +1,6 @@
+create database campeonatoBrasileiro;
+
+use campeonatoBrasileiro;
+
+insert into estadio (nome, endereco, capacidade) values 
+('Maracanã','AV.pres. Castelo Branco, portão 3 - Maracanã, Rio de Janeiro - RJ',78838);
