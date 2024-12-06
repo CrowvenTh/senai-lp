3c1fba2d2af65288fd148104e8ab460a057ed5ec
ThiagoSilva <thiagoantonio2108@gmail.com>
2024-12-05 21:48:52 -0300

    insert rodada 37

--- bd/Exercicios/campeonatoBrasileiro/QueryBrasileiraoExercicios.sql
+++ bd/Exercicios/campeonatoBrasileiro/QueryBrasileiraoExercicios.sql
@@ -1,16 +1,80 @@
 
+use campeonatobrasileiro;
+
 -- 01. Atualize na tabela evento as descrições 
 -- DE "Substitution" PARA "Substituição"
 -- DE "Woodwork" PARA "Bola na Trave"
 -- DE "Cartão Vermelho (Second Yellow Card)" PARA "Cartão Vermelho (Segundo Cartão Amarelo)"
 
--- 02. Atualize o resultado das partidas do brasileirão 
+select * from campeonatobrasileiro.evento;
+update campeonatobrasileiro.evento set descricao = "Substituição" where descricao = "Substitution";
+update campeonatobrasileiro.evento set descricao = "Bola na Trave" where descricao = "Woodwork";
+update campeonatobrasileiro.evento set descricao = "Cartão Vermelho (Segundo Cartão Amarelo)" where descricao = "Cartão Vermelho (Second Yellow Card)";
+
+-- 02. Atualize o resultado das partidas do brasileirão
 -- que já aconteceram e ainda não foi gravado o resultado.
+select id_partida , rodada, tm.sigla, gol_mandante, tv.sigla, gol_visitante 
+	from partida p 
+	inner join time tm 
+		on id_mandante = tm.id_time
+	inner join time tv 
+		on id_visitante = tv.id_time
+	where gol_mandante is null or rodada >= 35
+order by id_partida;
+
+update partida set gol_mandante = 0, gol_visitante = 0 where id_partida = 0;
+-- rodada 35
+update partida set gol_mandante = 1, gol_visitante = 1 where id_partida = 341;
+update partida set gol_mandante = 0, gol_visitante = 1 where id_partida = 342;
+update partida set gol_mandante = 1, gol_visitante = 1 where id_partida = 343;
+update partida set gol_mandante = 2, gol_visitante = 2 where id_partida = 344;
+update partida set gol_mandante = 4, gol_visitante = 1 where id_partida = 345;
+update partida set gol_mandante = 1, gol_visitante = 1 where id_partida = 346;
+update partida set gol_mandante = 3, gol_visitante = 1 where id_partida = 347;
+update partida set gol_mandante = 0, gol_visitante = 0 where id_partida = 348;
+update partida set gol_mandante = 0, gol_visitante = 0 where id_partida = 349;
+update partida set gol_mandante = 1, gol_visitante = 1 where id_partida = 350;
+-- rodada 36
+update partida set gol_mandante = 2, gol_visitante = 3 where id_partida = 351;
+update partida set gol_mandante = 3, gol_visitante = 1 where id_partida = 352;
+update partida set gol_mandante = 1, gol_visitante = 2 where id_partida = 353;
+update partida set gol_mandante = 2, gol_visitante = 4 where id_partida = 354;
+update partida set gol_mandante = 2, gol_visitante = 2 where id_partida = 355;
+update partida set gol_mandante = 2, gol_visitante = 1 where id_partida = 356;
+update partida set gol_mandante = 3, gol_visitante = 2 where id_partida = 357;
+update partida set gol_mandante = 2, gol_visitante = 0 where id_partida = 358;
+update partida set gol_mandante = 1, gol_visitante = 1 where id_partida = 359;
+update partida set gol_mandante = 1, gol_visitante = 1 where id_partida = 360;
+-- rodada 37
+update partida set gol_mandante = 0, gol_visitante = 1 where id_partida = 361;
+update partida set gol_mandante = 1, gol_visitante = 2 where id_partida = 362;
+update partida set gol_mandante = 1, gol_visitante = 1 where id_partida = 363;
+update partida set gol_mandante = 1, gol_visitante = 1 where id_partida = 364; -- indeifindo
+update partida set gol_mandante = 2, gol_visitante = 0 where id_partida = 365;
+update partida set gol_mandante = 3, gol_visitante = 0 where id_partida = 366;
+update partida set gol_mandante = 1, gol_visitante = 2 where id_partida = 367;
+update partida set gol_mandante = 0, gol_visitante = 1 where id_partida = 368; -- indefinido
+update partida set gol_mandante = 3, gol_visitante = 1 where id_partida = 369;
+update partida set gol_mandante = 0, gol_visitante = 3 where id_partida = 370;
+
 
 -- 03. Elabore um relatório por minuto e a quantidade de gols (não contar "Gol anulado (Var)")
 -- e ordene pela quantidade do maior para o menor
+select * from evento;
+select minuto, 
+	(select count(descricao) 
+		where descricao = "Gol (Gol de campo)" 
+	group by 1) 
+	from evento;
+	
 
 -- 04. Elabore um relatório por idade e quantidade de jogadores
+show tables;
+select (year(curdate()) - year(dt_nascimento)) as idade, 
+		count(id_jogador) jogadores 
+	from jogador
+	group by 1;
+-- order by idade desc;
 
 -- 05. Elabore um relatório por jogador e quantidade de cartões, 
 -- detalhar também a quantidade de Cartões Vermelho e Amarelo
