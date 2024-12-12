1ed2e22cff7461c7586e96ac180ad5639d0dab1c
ThiagoSilva <thiagoantonio2108@gmail.com>
2024-12-12 19:56:18 -0300

    primeiro exemplo de procedure

---
+++ bd/Rotinas/Procedures/exemploProcedure.sql
@@ -0,0 +1,23 @@
+use campeonatobrasileiro;
+
+create table usuarios (
+ id int not null auto_increment, 
+ nome varchar(100) default null,
+ email varchar(100) default null,
+ 	primary key (id)
+);
+
+delimiter $$
+
+create procedure adicionar_usuario(nome varchar(100), email varchar(100))
+begin 
+	insert into usuarios (nome, email) values (nome, email);
+end$$
+
+delimiter ;
+
+call adicionar_usuario('João Silva','joao@gmail.com');
+call adicionar_usuario('Luiz Felipe','luiz@gmail.com');
+call adicionar_usuario('Elon Musk','elon@gmail.com');
+
+select * from usuarios;
