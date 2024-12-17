29be58132273bf20e81eab436a4356a0f94edb76
ThiagoSilva <thiagoantonio2108@gmail.com>
2024-12-16 21:12:17 -0300

    exercicios Function, resolução 1

--- bd/Rotinas/Functions/ExerciciosFunction.sql
+++ bd/Rotinas/Functions/ExerciciosFunction.sql
@@ -1,18 +1,29 @@
-/*
-01. function valor_aleatorio
-Implemente uma função no MySQL chamada valor_aleatorio que receba dois parâmetros inteiros: min (valor mínimo) e max (valor máximo). A função deve retornar um número inteiro aleatório dentro do intervalo fechado definido pelos parâmetros fornecidos, ou seja, o valor retornado deve estar entre min e max (inclusive).
+-- RESOLUÇÃO 01 -- 
 
-Requisitos:
+drop function valor_aleatorio;
 
-A função deve ser not determinística, pois cada execução pode gerar um valor diferente.
-Utilize a função interna RAND() do MySQL para gerar números aleatórios.
-Garanta que o número retornado seja arredondado para o inteiro mais próximo, sem casas decimais.
-Valide os cálculos de modo que o número gerado esteja adequadamente dentro do intervalo fornecido pelos parâmetros.
-select valor_aleatorio(5,15);
-select valor_aleatorio(5,15);
-select valor_aleatorio(5,15);
-select valor_aleatorio(5,15);
-select valor_aleatorio(5,15);
-select valor_aleatorio(5,15);
-select valor_aleatorio(5,15);
-*/
+delimiter $$
+
+create function valor_aleatorio(valorMin int, valorMax int)
+returns int 
+not deterministic
+reads sql data
+begin 
+	return round(rand() * (valorMax - valorMin)) + valorMin;
+end$$
+
+delimiter ;
+
+select valor_aleatorio(5, 15) as "valor aleatorio";
+select valor_aleatorio(15, 20) as "valor aleatorio";
+select valor_aleatorio(1, 5) as "valor aleatorio";
+
+-- -- -- 
+
+-- RESOLUÇÃO 02 --
+
+-- -- -- 
+
+-- RESOLUÇÃO 03 --
+
+-- -- --
