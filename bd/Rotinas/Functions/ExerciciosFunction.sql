5be9693a52d1481e68ca5add4a1308e3ef8a1ff6
ThiagoSilva <thiagoantonio2108@gmail.com>
2024-12-16 21:36:45 -0300

    Resolução exercicio de function 02

--- bd/Rotinas/Functions/ExerciciosFunction.sql
+++ bd/Rotinas/Functions/ExerciciosFunction.sql
@@ -21,6 +21,18 @@ select valor_aleatorio(1, 5) as "valor aleatorio";
 -- -- -- 
 
 -- RESOLUÇÃO 02 --
+delimiter $$
+create function data_br(dataBr date)
+returns varchar(20)
+deterministic 
+begin
+	return date_format(dataBr,'%d/%m/%Y');
+end$$
+
+delimiter ;
+
+select data_br(curdate()) as 'Dia de hoje';
+select data_br('2004-08-21');
 
 -- -- -- 
 
