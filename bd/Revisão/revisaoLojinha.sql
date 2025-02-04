55e447867e205a537232207279f28aea2164bbbe
ThiagoSilva <thiagoantonio2108@gmail.com>
2025-02-04 19:49:33 -0300

    criacao de tabelas revisaoLojinha

--- bd/Revisão/revisaoLojinha.sql
+++ bd/Revisão/revisaoLojinha.sql
@@ -26,3 +26,36 @@ id_cliente int,
 	constraint PK_pedido primary key(id),
 	constraint FK_pedido_cliente foreign key(id_cliente) references cliente(id)
 );
+
+create table categoria(
+id int,
+nome varchar(100),
+ constraint PK_categoria primary key(id)
+);
+
+create table fornecedor(
+id int,
+nome varchar(100),
+ constraint PK_fornecedor primary key(id)
+);
+
+create table produto (
+id int,
+descricao varchar(100),
+idCategoria int,
+idFornecedor int,
+estoque int,
+	constraint PK_idProduto primary key(id),
+	constraint FK_id_categoria foreign key(idCategoria) references categoria(id),
+	constraint FK_id_Fornecedor foreign key(idFornecedor) references fornecedor(id)
+);
+
+create table pedido_produto (
+id int,
+idPedido int,
+idProduto int,
+quantidade int,
+	constraint PK_pedido_produto primary key(id),
+	constraint FK_id_pedido foreign key(idPedido) references pedido(id),
+	constraint FK_id_produto foreign key(idProduto) references produto(id)
+);
