84b6f8b0150c4b27634d197feea5e91d88f85bb2
ThiagoSilva <thiagoantonio2108@gmail.com>
2025-02-04 20:03:44 -0300

    insert das tabelas revisaoLojinha

--- bd/Revisão/revisaoLojinha.sql
+++ bd/Revisão/revisaoLojinha.sql
@@ -9,7 +9,6 @@ constraint PK_cliente primary key(id)
 );
 
 desc cliente;
-select * from cliente;
 
 create table email_cliente (
 id int,
@@ -59,3 +58,46 @@ quantidade int,
 	constraint FK_id_pedido foreign key(idPedido) references pedido(id),
 	constraint FK_id_produto foreign key(idProduto) references produto(id)
 );
+
+insert into cliente values 
+(01, "Ana"),
+(02, "José"),
+(03, "João");
+
+insert into email_cliente values
+(01,"e1",1),
+(02,"e2",1),
+(03,"e3",2),
+(04,"e4",2),
+(05,"e5",3);
+
+insert into pedido values
+(01, "2015-10-05", 1),
+(02, "2014-12-31", 1),
+(03, "2010-09-10", 2),
+(04, "2015-03-14", 2),
+(05, "2014-07-21", 2);
+
+insert into categoria values
+(01,"Ferramenta"),
+(02,"Vestuário"),
+(03,"Alimento");
+
+insert into fornecedor values
+(01,"F1"),
+(02,"F2"),
+(03,"F3");
+
+insert into produto values
+(01, "Alicate", 1, 1, 10),
+(02, "Leite", 2, 1, 100),
+(03, "Suco", 2, 1, 50),
+(04, "Camisa", 3, 2, 40),
+(05, "Calça", 3, 2, 20);
+
+insert into pedido_produto values
+(01, 01, 01, 10),
+(02, 01, 02, 20),
+(03, 02, 03, 5),
+(04, 03, 04, 10),
+(05, 04, 05, 5);
